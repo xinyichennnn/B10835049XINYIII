16409f0f52f2431c11fc5b2006cd997c7651c508
xinyichennnn <79498052+xinyichennnn@users.noreply.github.com>
2021-03-22 15:13:19 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let x = 0;
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  
+arc(mouseX, 55, 50, 50, 0, HALF_PI);
+noFill();
+arc(50, 55, 60, 60, HALF_PI, PI);
+arc(50, 55, 70, 70, PI, PI + QUARTER_PI);
+arc(50, 55, 80, 80, PI + QUARTER_PI, TWO_PI);
+}
